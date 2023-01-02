df0b6c593995537e0fa8e4a7ff1f9f0c5b7b8bfd
Zazikni <nikitachepelskii2@gmail.com>
2023-01-02 14:06:49 +0300

    create Task class

--- Task.cs
+++ Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,33 @@ namespace TASKMANAGER
     internal class Task
     {
         private int taskId;
-        private string taskName;
+        private string taskDescription;
+        private string taskStatus;
+        private string taskOwner;
+        private string taskCreator;
+        public Task (string TaskDescription, string TaskStatus , string TaskOwner, string TaskCreator)
+        {
+            taskDescription = TaskDescription;
+            taskStatus = TaskStatus;
+            taskOwner = TaskOwner;
+            taskCreator = TaskCreator;
+            using (StreamReader file = new StreamReader(@"nextTaskIdId.txt"))
+            {
+                taskId = Convert.ToInt32(file.ReadLine());
+            }
+            using (StreamWriter file = new StreamWriter(@"nextTaskIdId.txt", false))
+            {
+                file.Write(++taskId);
+            }
+        }
+        public string TaskStatus
+        {
+            get { return taskStatus; }
+            set { taskStatus = value; }
+        }
+        public override string ToString()
+        {
+            return $"{taskDescription}";
+        }
     }
 }
